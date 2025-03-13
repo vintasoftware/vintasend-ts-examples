2a4993f7d8ccd653db40867a28b383080196762e
Hugo bessa <hugo@vinta.com.br>
2025-03-13 10:27:56 -0300

    Bump vintasend version

--- nextjs-prisma-nodemailer-pug-temporal/src/lib/services/notifications.ts
+++ nextjs-prisma-nodemailer-pug-temporal/src/lib/services/notifications.ts
@@ -1,8 +1,8 @@
 import { PrismaClient } from '@prisma/client';
-import { NotificationService, NotificationContextRegistry } from 'vintasend/dist/index.js';
-import { PrismaNotificationBackend } from 'vintasend-prisma/dist/index.js';
-import { PugEmailTemplateRenderer } from 'vintasend-pug/dist/index.js';
-import { NodemailerNotificationAdapter } from 'vintasend-nodemailer/dist/index.js';
+import { VintaSendFactory } from 'vintasend/dist/index.js';
+import { PrismaNotificationBackendFactory } from 'vintasend-prisma/dist/index.js';
+import { PugEmailTemplateRendererFactory } from 'vintasend-pug/dist/index.js';
+import { NodemailerNotificationAdapterFactory } from 'vintasend-nodemailer/dist/index.js';
 import { WinstonLogger } from 'vintasend-winston/dist/index.js';
 import { ForgotPasswordContextGenerator } from '../../app/api/auth/forgot-password/forgot-password-notification-context';
 import { EmailVerificationNotificationContextGenerator } from '../../app/api/auth/signup/email-verification-notification-context';
@@ -10,7 +10,6 @@ import { FirstDayotificationContextGenerator } from '../../app/api/auth/signup/f
 import { loggerOptions } from '../logger';
 import type { Notification, User } from '@prisma/client';
 import type SMTPTransport from 'nodemailer/lib/smtp-transport/index.js';
-import type { BaseNotificationTypeConfig } from 'vintasend/dist/types/notification-type-config';
 
 export const contextGeneratorsMap = {
   forgotPassword: new ForgotPasswordContextGenerator(),
@@ -18,38 +17,36 @@ export const contextGeneratorsMap = {
   firstDay: new FirstDayotificationContextGenerator(),
 } as const;
 
-export type ContextMap = typeof contextGeneratorsMap;
-
-export type NotificationTypeConfig = BaseNotificationTypeConfig & {
-  ContextMap: ContextMap;
+export type NotificationTypeConfig = {
+  ContextMap: typeof contextGeneratorsMap;
   NotificationIdType: Notification['id'];
   UserIdType: User['id'];
 };
 
 export function getNotificationService() {
-  const prisma = new PrismaClient();
-  const notificationBackend = new PrismaNotificationBackend<
-    PrismaClient,
-    NotificationTypeConfig
-  >(prisma);
-  const pugEmailTemplateRenderer = new PugEmailTemplateRenderer<NotificationTypeConfig>();
-  const nodemailerNotificationAdapter = new NodemailerNotificationAdapter<
-    PugEmailTemplateRenderer<NotificationTypeConfig>,
-    NotificationTypeConfig
-  >(pugEmailTemplateRenderer, true, {
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT || 587,
-    secure: false, // true for port 465, false for other ports
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASSWORD,
-    },
-  } as SMTPTransport.Options)
-  return new NotificationService<NotificationTypeConfig>(
+  const notificationBackend = new PrismaNotificationBackendFactory<NotificationTypeConfig>().create(
+    new PrismaClient(),
+  );
+  const pugEmailTemplateRenderer =
+    new PugEmailTemplateRendererFactory<NotificationTypeConfig>().create({});
+  const nodemailerNotificationAdapter =
+    new NodemailerNotificationAdapterFactory<NotificationTypeConfig>().create(
+      pugEmailTemplateRenderer,
+      true,
+      {
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT || 587,
+        secure: false, // true for port 465, false for other ports
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASSWORD,
+        },
+      } as SMTPTransport.Options,
+    );
+  return new VintaSendFactory<NotificationTypeConfig>().create(
     [nodemailerNotificationAdapter],
     notificationBackend,
     new WinstonLogger(loggerOptions),
+    contextGeneratorsMap,
   );
 }
-
-NotificationContextRegistry.initialize<ContextMap>(contextGeneratorsMap);
